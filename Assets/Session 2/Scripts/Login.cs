7fecf07a6f92282ab5511e619d407a52d9a8d792
YashUmeshPatel <yashpatel@yudiz.com>
2023-03-06 18:52:31 +0530

    Login Page

---
+++ Assets/Session 2/Scripts/Login.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    public void Login_OnClick()
+    {
+        
+    }
+}
